58d1bcc803494b88c397126eb92cecce5d829a06
Tanawit Khunha <tanawit.3133@gmail.com>
2021-05-18 01:07:43 +0700

    edit footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,26 +5,26 @@ export default function Footer() {
     <footer>
       <div className="container-footer">
         <div className="footer-row">
-          <a href="#" className="footer-logo">
+          <a href="https://seemoon.io/" className="footer-logo">
             <img src="img/logo-footer.png" />
           </a>
           <div className="footer-social">
-            <a href="#">
+            <a href="https://www.facebook.com/">
               <i className="fab fa-facebook-f" />
             </a>
-            <a href="#">
+            <a href="https://www.instagram.com/">
               <i className="fab fa-instagram" />
             </a>
-            <a href="#">
+            <a href="https://www.youtube.com/">
               <i className="fab fa-youtube" />
             </a>
-            <a href="#">
+            <a href="https://twitter.com/">
               <i className="fab fa-twitter" />
             </a>
-            <a href="#">
+            <a href="https://telegram.org/">
               <i className="fab fa-telegram-plane" />
             </a>
-            <a href="#">
+            <a href="https://www.reddit.com/">
               <i className="fab fa-reddit-alien" />
             </a>
           </div>
